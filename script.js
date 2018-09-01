ba9402ff2e9da792346af0ff097552dad2301dae
jonathanacevedo <ozzhed@gmail.com>
2018-09-01 09:55:11 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let taskApp = new Vue({
+    el: '#taskApp',
+    data: {
+        tasks: [
+        ]
+    },
+    mounted() {
+        this.$http.get("http://142.93.202.10:8080/tasks").then(result => {
+            this.tasks = result.data;
+        }, error => {
+            console.error(error);
+        });
+    },
+    methods: {
+        addTask: function(e){
+            e.preventDefault();
+
+            if(this.tasks.name){
+                let task = {
+                    name: this.tasks.name,
+                    done: false
+                }
+                this.$http.post("http://142.93.202.10:8080/tasks", task).then(result => {
+                    this.tasks.push(result.data);
+                    console.log('Todo salio bien '+this.tasks);
+                }, error => {
+                    console.error('Error: '+error);
+                });
+                this.tasks.name = "";
+            }
+        },
+
+        deleteTask: function (task){
+            this.$http.delete("http://142.93.202.10:8080/tasks", {body: {id: task.id}})
+            .then(result => {
+                console.log(JSON.stringify(result));
+                this.tasks.splice(this.tasks.indexOf(task),1);
+
+            }, error => {
+                console.error('Error: '+error);
+            });
+        },
+        updateTask: function(task) {
+            this.$http.put("http://142.93.202.10:8080/tasks", task).then(result => {
+                console.log(result);
+                this.tasks = result.data;
+            }, error => {
+                console.error('Error '+error);
+            });
+        }
+    }
+});
